02fce134f2385777bc1216696b9f061d0899c0a1
Huy <tranduchuypro@gmail.com>
2024-04-12 21:23:24 +0700

    add MainCharInfo modal

---
+++ src/classes/MainCharInfo.java
@@ -0,0 +1,25 @@
+package classes;
+
+public class MainCharInfo {
+
+	public static String getPlayerName() {
+		return class_acv.s.q.a_();
+	}
+
+	public static int getDoBen() {
+		return ModScr.getDoBen();
+	}
+
+	public static long getCurrentCoin() {
+		return class_acv.s.q.br;
+	}
+
+	public static int getCurrentGold() {
+		return class_acv.s.q.aV;
+	}
+
+	public static int getCurrentLockGold() {
+		return class_acv.s.q.aW;
+	}
+
+}
