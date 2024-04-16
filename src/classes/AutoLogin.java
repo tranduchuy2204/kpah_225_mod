87167aea06bbee4e2f6d91b6f1dce4aa27e421a1
Huy <tranduchuypro@gmail.com>
2024-04-17 02:04:13 +0700

    add auto login feature (none coding)

---
+++ src/classes/AutoLogin.java
@@ -0,0 +1,33 @@
+package classes;
+
+public interface AutoLogin {
+
+	void onDisconnected();
+
+	void startAuto();
+
+}
+
+class AutoLoginV1 implements AutoLogin {
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void startAuto() {
+		// TODO Auto-generated method stub
+
+	}
+}
+
+class AutoLoginV2 implements AutoLogin {
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void startAuto() {
+		// TODO Auto-generated method stub
+
+	}
+}
