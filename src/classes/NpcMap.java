7598a9d74282977d6452f62c39d17a34eb8a8639
Huy <tranduchuypro@gmail.com>
2024-04-16 23:04:53 +0700

    refactoring code

--- src/classes/NpcMap.java
+++ src/classes/NpcMap.java
@@ -11,20 +11,20 @@ package classes;
  */
 public class NpcMap implements IAction {
 
-    int[] cmd;
+	int[] cmd;
 
-    public NpcMap(int[] cmd) {
-        this.cmd = cmd;
-    }
+	public NpcMap(int[] cmd) {
+		this.cmd = cmd;
+	}
 
-    public void perform() {
-        if (class_acv.s.aG == cmd[1]) {
-            return;
-        }
-        class_gm.a().b(cmd[0], cmd[2], cmd[3]);
-        class_go.a().b(cmd[1], cmd[2], cmd[3]);
+	public void perform() {
+		if (class_acv.s.aG == cmd[1]) {
+			return;
+		}
+		class_gm.a().b(cmd[0], cmd[2], cmd[3]);
+		class_go.a().b(cmd[1], cmd[2], cmd[3]);
 		class_abj.cw = class_acv.s.aG;
-        class_yi.g();
-    }
+		class_yi.g();
+	}
 
 }
